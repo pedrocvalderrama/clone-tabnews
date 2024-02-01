f3082623fe51867847ad6955f7b323c731147670
pedrocvalderrama <157759992+pedrocvalderrama@users.noreply.github.com>
2024-02-01 00:23:38 +0000

    adiciona scripts `lint:check` e `lnt:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-    return <h1>Te Amo Mozaaaaaaaaaaaaaaaaaaaao da minha viiiiiiiiiiiiiiiiiiiiiiiiiiidaaaa</h1>
+  return (
+    <h1>
+      Te Amo Mozaaaaaaaaaaaaaaaaaaaao da minha viiiiiiiiiiiiiiiiiiiiiiiiiiidaaaa
+    </h1>
+  );
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
